2be513a842bea8938c9283b870e1d83abaa6d60f
SJCMN <sam.clavette@gmail.com>
2021-11-07 16:50:02 -0600

    merge master

--- src/components/AddMoviePage/AddMoviePage.jsx
+++ src/components/AddMoviePage/AddMoviePage.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
 function AddMoviePage () {
+    const history = useHistory();
+
     return(
         <div>
 
+        <button onClick={() => history.push('/')}>Back to List</button>
         </div>
     );
 };
